05086868daf311917cb338371ff40035e7a6b689
YdrMaster <ydrml@hotmail.com>
2019-04-01 09:39:26 +0800

    修复实例代码

--- src/sample/test_io.cpp
+++ src/sample/test_io.cpp
@@ -2,24 +2,15 @@
 #include <thread>
 #include "../main/serial_port.hh"
 
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wmissing-noreturn"
-
 int main() {
 	try {
-		serial_port port("COM3", 115200);
+		serial_port port("COM4", 115200);
 		
 		std::thread([&port] {
 			uint8_t buffer[128]{};
 			while (true) {
 				auto actual = port.read(buffer, sizeof(buffer) - 1);
-				//				std::cout << std::string(buffer, buffer + actual) << std::endl;
-			}
-		}).detach();
-		
-		std::thread([&port] {
-			while (true) {
-				std::cerr << port.test() << std::endl;
+				std::cout << std::string(buffer, buffer + actual) << std::endl;
 			}
 		}).detach();
 		
@@ -32,5 +23,3 @@ int main() {
 		std::cerr << e.what() << std::endl;
 	}
 }
-
-#pragma clang diagnostic pop
