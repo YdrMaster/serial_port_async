3ca57fda7be55d47ac11eb7a966626b514a93b92
YangDR <ydrml@hotmail.com>
2019-05-22 03:31:40 -0700

    有毒

--- src/sample/test_io.cpp
+++ src/sample/test_io.cpp
@@ -4,23 +4,25 @@
 #include "../main/serial_port.hh"
 
 int main() {
-	try {
-        serial_port port("/dev/ttyUSB0", 115200);
-		
-		std::thread([&port] {
-			uint8_t buffer[128]{};
-			while (true) {
-				auto actual = port.read(buffer, sizeof(buffer) - 1);
-				std::cout << std::string(buffer, buffer + actual) << std::endl;
-			}
-		}).detach();
-		
-		const auto text = "abcde";
+    try {
+        serial_port port("/dev/ttyUSB0", 115200, 3, 1);
+        
+        std::thread([&port] {
+            uint8_t buffer[128]{};
+            while (true) {
+                auto     actual = port.read(buffer, sizeof(buffer) - 1);
+                for (int i      = 0; i < actual; ++i)
+                    std::cout << (int) buffer[i] << " ";
+                std::cout << std::endl;
+            }
+        }).detach();
+        
+        const uint8_t text[]{0x2d, 11, 10, 13};
         while (true) {
-            port.send((uint8_t *) text, std::strlen(text));
-            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+            port.send(text, sizeof(text));
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
         }
-	} catch (std::exception &e) {
-		std::cerr << e.what() << std::endl;
-	}
+    } catch (std::exception &e) {
+        std::cerr << e.what() << std::endl;
+    }
 }
